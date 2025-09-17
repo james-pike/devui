f72f0a8cc1838e67ae3c96b2e525399d2ff34228
your name <you@exammple.com>
2025-09-17 11:24:31 -0400

    commit

--- src/components/widgets/Services.tsx
+++ src/components/widgets/Services.tsx
@@ -4,7 +4,6 @@ import { Wrapper } from './Wrapper';
 
 import styles from './carousel.css?inline';
 import ServicesTabs from './ServicesTabs';
-import { Card } from '../ui/Card';
 
 export default component$(() => {
   useStyles$(styles);
@@ -15,21 +14,10 @@ export default component$(() => {
     <>
       <Wrapper>
     
-    <Card.Root class=" max-w-3xl mx-auto">
-        <Card.Header class=" text-center bg-primary/5">
-<Card.Title class="text-xl">
-Our Services
-    </Card.Title>
-    <Card.Description class="text-md">
-Premium web design, development, branding and marketing.
-    </Card.Description>
-
-
-        </Card.Header>
+       
        
      <ServicesTabs/>
   
-     </Card.Root>
       </Wrapper>
     </>
   );
