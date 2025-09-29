e9f5bebe38a65ac70fcffe6e28961b4fdd18ac9c
your name <you@exammple.com>
2025-09-29 12:51:30 -0400

    commit

--- src/components/ui/Separator.tsx
+++ src/components/ui/Separator.tsx
@@ -11,7 +11,7 @@ export const Separator = component$<PropsOf<typeof HeadlessSeparator>>(
           decorative={decorative}
           orientation={orientation}
           class={cn(
-            'shrink-0 bg-primary/15',
+            'shrink-0 bg-primary/10',
             orientation === 'horizontal' ? 'h-px w-full' : 'h-full w-px',
             props.class,
           )}
